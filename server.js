4cd731d390216ad07140fdbc2df5f305000d7d0e
knishihama <47240829+knishihama@users.noreply.github.com>
2019-02-24 23:56:12 +0900

    ビルド成功したので、コメントアウトなど不要な箇所を解除

--- server.js
+++ server.js
@@ -3,11 +3,8 @@ const {Nuxt} = require('nuxt')
 const app = express()
 const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || '3000'
-//const port = '3000'
 // Import and set Nuxt.js options
 let config = require('./nuxt.config.js')
-console.log('[DEBUG]host:' + host);
-console.log('[DEBUG]port:' + port);
 const nuxt = new Nuxt(config)
 // Give Nuxt middleware to express
 app.use(nuxt.render)
