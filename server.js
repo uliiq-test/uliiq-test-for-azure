c7c32a584c44ee0fdbef93fa7d4b7dbc5377687b
knishihama <47240829+knishihama@users.noreply.github.com>
2019-02-24 23:25:44 +0900

    ポート番号を試しに固定値にする

--- server.js
+++ server.js
@@ -2,17 +2,15 @@ const express = require('express')
 const {Nuxt} = require('nuxt')
 const app = express()
 const host = process.env.HOST || '127.0.0.1'
-const port = process.env.PORT || '3000'
-console.log('host:' + host);
-console.log('port:' + port);
+//const port = process.env.PORT || '3000'
+const port = '3000'
 // Import and set Nuxt.js options
 let config = require('./nuxt.config.js')
-console.log('(nuxt.config.js)host:' + host);
-console.log('(nuxt.config.js)port:' + port);
+console.log('[DEBUG]host:' + host);
+console.log('[DEBUG]port:' + port);
 const nuxt = new Nuxt(config)
 // Give Nuxt middleware to express
 app.use(nuxt.render)
-console.log('nuxt.render:' + nuxt.render);
 // Listen the server
 app.listen(port, host);
 console.log('Server listening on ' + host + ':' + port);
