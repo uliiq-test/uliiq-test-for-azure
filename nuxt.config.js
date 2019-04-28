a25350fe0bfc3456d7b85d10ff4b9c0efc51444a
Kenji Nishihama <47240829+knishihama@users.noreply.github.com>
2019-04-28 17:56:24 +0900

    no message

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,6 @@ const parseArgs = require('minimist')
 
 const environment = process.env.NODE_ENV || 'test';
 const envSet = require(`./env.${environment}.js`)
-console.log(`./env.${environment}.js`);
 
 const argv = parseArgs(process.argv.slice(2), {
   alias: {
