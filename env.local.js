6e48615aaf07faba2c2c1c3efb13c27525ab186e
knishihama <47240829+knishihama@users.noreply.github.com>
2019-06-02 13:13:15 +0900

    認証確認

---
+++ env.local.js
@@ -0,0 +1,12 @@
+module.exports = {
+    GTAG_ID: null,
+    WEB_URL: 'http://localhost:3000',
+    INSTANCE: 'a59705a8-0ef5-451d-8390-983b318cf9d7',
+    CLIENT_ID: 'https://login.microsoftonline.com/tfp',
+    DOMAIN: 'wishhub.onmicrosoft.com',
+    SIGN_UP_SIGN_IN_POLICY_ID: 'B2C_1_wishhub_dev',
+    LOGIN_REDIRECT_URL: 'http://localhost:3000/account/signup',
+    LOGOUT_REDIRECT_URL: 'http://localhost:3000',
+    API_URL: 'https://wishhub-dev.azurewebsites.net',
+    FB_APP_ID: ''
+}
---
+++ env.uliiq.js
@@ -0,0 +1,12 @@
+module.exports = {
+    GTAG_ID: null,
+    WEB_URL:'https://uliiq-wishhub.azurewebsites.net',
+    INSTANCE: '482e06d9-31d1-44c7-a9da-ee5a3bb837da',
+    CLIENT_ID: 'https://login.microsoftonline.com/tfp',
+    DOMAIN: 'uliiqauth.onmicrosoft.com',
+    SIGN_UP_SIGN_IN_POLICY_ID: 'B2C_1_wishhub_uliiq',
+    LOGIN_REDIRECT_URL: 'https://uliiq-wishhub.azurewebsites.net/account/signup', //ダミー
+    LOGOUT_REDIRECT_URL: 'https://uliiq-wishhub.azurewebsites.net',
+    API_URL: 'https://wishhub-dev-api-uliiq.azurewebsites.net',
+    FB_APP_ID: ''
+}
